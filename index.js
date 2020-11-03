a7d9a9c143c18090de15de6e9a21c09ed14b644f
Raelene Ho <raeyean@gmail.com>
2020-11-03 21:44:11 +1100

    added javascript function

---
+++ index.js
@@ -0,0 +1,27 @@
+// Image 1 selection
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; // 1-6
+
+var randomDiceImage = 'dice' + randomNumber1 + '.png'; // dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; // images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll('img')[0];
+
+image1.setAttribute("src", randomImageSource);
+
+// Image 2 selection
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + '.png';
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+// Update title to display winner
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").textContent = "Player 1 wins!"
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").textContent = "Player 2 wins!"
+} else {
+  document.querySelector("h1").textContent = "Draw"
+}
